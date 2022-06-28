708949e621fd8b1f48f9ae4eb7fa3c0b77e1c77b
Dat T Nguyen <datn@Dats-MacBook-Pro.local>
2022-06-28 12:03:46 +0200

    update fig 5

--- Figure_5.R
+++ Figure_5.R
@@ -1,6 +1,6 @@
 
 setwd("/Users/datn/github/SNP_array_comparison")
-setwd("data/pmra_real_data")
+setwd("data/real_data")
 
 require(scales)
 require(ggplot2)
@@ -27,35 +27,59 @@ get_mean_imp_acc <- function(df, cutoffs, flag = "flag"){
   return(rdf)
 }
 
-res = list()
 
-for(i in 1:22){
-  # real
-
-  x = fread( paste0("95_shared_sample_pmra/chr", i , "_imputed.correlation.txt.gz"))
-  res[[i]] =  get_mean_imp_acc(x, cutoffs, "Real_PMRA")
-
+get_imp_acc_array <- function(array_path, flag){
+  res = list()
+  cutoffs = list("(0-0.01]" = c(0, 0.01), "(0.01-0.05]" = c(0.01, 0.05), "(0.01-0.5]" = c(0.01, 0.5),  "(0.05-0.5]" = c(0.05,0.5))
+  for(i in 1:22){
+    x = fread( paste0(array_path, "/chr", i , "_imputed.correlation.txt.gz"))
+    res[[i]] =  get_mean_imp_acc(x, cutoffs, flag)
+  }
+  df = do.call("rbind", res)
+  df = as.data.frame(df)
+  names(df) = c("imp_acc", "Bin", "Type")
+  df$imp_acc = as.numeric(df$imp_acc)
+  return(df)
 }
 
-df1 = do.call("rbind", res)
+
+
+# res = list()
+
+# for(i in 1:22){
+#   # real
+
+#   x = fread( paste0("95_shared_sample_pmra/chr", i , "_imputed.correlation.txt.gz"))
+#   res[[i]] =  get_mean_imp_acc(x, cutoffs, "Real_PMRA")
+
+# }
+
+# df1 = do.call("rbind", res)
 
 
 
-res = list()
+# res = list()
 
-for(i in 1:22){
-  # simulated
+# for(i in 1:22){
+#   # simulated
 
-  x = fread( paste0("95_shared_sample_simulated/chr", i , "_imputed.correlation.txt.gz"))
-  res[[i]] =  get_mean_imp_acc(x, cutoffs, "Simulated_PMRA")
-}
+#   x = fread( paste0("95_shared_sample_simulated/chr", i , "_imputed.correlation.txt.gz"))
+#   res[[i]] =  get_mean_imp_acc(x, cutoffs, "Simulated_PMRA")
+# }
 
-df2 = do.call("rbind", res)
+# df2 = do.call("rbind", res)
 
-df = as.data.frame(rbind(df1, df2))
-names(df) = c("imp_acc", "Bin", "Type")
+df1 = get_imp_acc_array("pmra_real_array", "PMRA_Real")
+df2 = get_imp_acc_array("pmra_simulated_array", "PMRA_Simulated")
 
-df$imp_acc = as.numeric(df$imp_acc)
+df3 = get_imp_acc_array("gsa_real_array", "GSA_Real")
+df4 = get_imp_acc_array("gsa_simulated_array", "GSA_Simulated")
+
+df = rbind(df1, df2, df3, df4)
+
+# names(df) = c("imp_acc", "Bin", "Type")
+
+# df$imp_acc = as.numeric(df$imp_acc)
 
 p1 <- ggplot(df, aes( y = imp_acc, x = Bin, color=  Type)) + geom_boxplot() + theme_light() + ylab("Mean imputation r2") + xlab("") + guides(x = guide_axis(angle = 90)) 
 
@@ -81,86 +105,10 @@ mean_imp_acc = round(mean_imp_acc,4)
 sd_imp_acc = round(sd_imp_acc,4)
 mean_sd = paste(mean_imp_acc, sd_imp_acc, sep = "±")
 stat_df = data.frame(Bin = bin, Imputation_accuracy = mean_sd)
-write.csv(stat_df, file = "../../output_paper/tables/Imputation_acc_real_simulated_PMRA.csv", quote = F, row.names = F)
+write.csv(stat_df, file = "../../output_paper/tables/Imputation_acc_real_simulated_pmra_gsa.csv", quote = F, row.names = F)
 
 
 
 
-# # p2
-
-
-
-
-# get_cor_array <- function(wgs, array, pop , trait){
-#   #wgs_path = paste0("WGS_", pop, "_", trait, ".all_score" )
-#   #wgs = fread()
-#   cols = names(wgs)[-c(1,2)]
-#   array_path = paste0("PRS_result/", array, "_", pop, "_", trait, ".all_score")
-#   arr = fread(array_path)
-#   res = c()
-#   for(col in cols){
-#     tem = cor(wgs[,..col], arr[,..col])
-#     res = c(res, tem)
-#   }
-#  # res2 = c(array, trait, res, mean(res), sd(res))
-#  # names(res2) = c("array", "trait", cols, "mean", "sd")
-#   res2 = c(res)
-#   names(res2) = c(cols)
-#   return(res2)
-# }
-
-
-
-# get_cor_pop <- function(pop, array_list, trait_list = c("HEIGHT", "BMI", "Type_2_diabetes")){
-
-#   res = list()
-
-#   for( trait in trait_list){
-#     wgs_path = paste0("PRS_result/95_shared_sample_wgs_", pop, "_", trait, ".all_score" )
-#     wgs = fread(wgs_path)
-#     all_cor = list()
-#     for( array in array_list){
-#     tem = get_cor_array(wgs, array, pop, trait)
-#     all_cor[[array]] = tem
-#     }
-#     res[[trait]] = as.data.frame(do.call(cbind, all_cor))
-#     res[[trait]]$trait = trait
-#   }
-#   df = as.data.frame(do.call(rbind, res))
-#   df$pop = pop
-#   return(df)
-# }
-
-
-# df = get_cor_pop("VNP", c("95_shared_sample_simulated", "95_shared_sample_pmra"))
-# names(df) = c("sim" , "real",      "trait"  ,                    "pop")
-
-# df$trait[df$trait == "HEIGHT"] = "Height"
-
-# p2 =  ggplot(df,aes(x = real, y = sim, color = trait)) +  geom_point() + theme_light() + ylab("Simulated PMRA") + xlab("Real PMRA")  + geom_smooth( aes(color = NULL), method=lm, se = F) + scale_y_continuous(breaks=seq(0,1,0.01), limits = c(0.93,0.985) ) + scale_x_continuous(breaks=seq(0,1,0.01), limits = c(0.93,0.985) ) 
-
-
-# # regEq <- function(lmObj, dig) {
-# #     paste0("y = ",
-# #         paste0(
-# #             c(round(lmObj$coef[1], dig), round(sign(lmObj$coef[-1])*lmObj$coef[-1], dig)),
-# #             c("", rep("*", length(lmObj$coef)-1)),
-# #             paste0(c("", names(lmObj$coef)[-1]), c(ifelse(sign(lmObj$coef)[-1]==1," + "," - "), "")),
-# #             collapse=""
-# #         )
-# #     )
-# # }
-
-# # m = lm(sim ~ real, data = df)
-# # eq = regEq(m,3)
-
-# t = paste0("R2: ", round( (cor(df[,1], df[,2]))^2,4))
-# p2 <- p2 + annotate(geom = "text", x=0.97, y=0.94, label= t)
-
-
-
-# pdf(file= "../Figure_6.pdf",  width=8, height=6)
-# p2
-# dev.off()
 
 
--- data/real_data/tem.R
+++ data/real_data/tem.R
@@ -1,5 +1,43 @@
 setwd("/Users/datn/github/SNP_array_comparison/data/real_data/PRS_result")
 
-fn = "gsa_real_array_VNP_BMI.all_score"
+get_prs_correlation_array <- function(array_name, wgs_name, flag){
+  
+  array_name = "gsa_real_array"
+  wgs_name = "wgs_24_sample"
+  trait = "HEIGHT"
+  flag = "GSA_Real"
+  
+  trait_list = c("HEIGHT", "BMI", "Type_2_diabetes")
+  res_trait = list()
+  for(trait in trait_list){
+    arr_prs_fn = paste0(array_name, "_VNP_", trait, ".all_score")
+    wgs_prs_fn = paste0(wgs_name, "_VNP_", trait, ".all_score")
+    
+    arr = fread(arr_prs_fn)
+    wgs = fread(wgs_prs_fn)
+    arr = as.data.frame(arr)
+    wgs = as.data.frame(wgs)
+    
+    cutoffs = names(arr)[-c(1:2)]
+    res = list()
+    for(c in cutoffs){
+      res[[c]] =  cor(arr[,c], wgs[,c], method = "pearson")
+    }
+    df = do.call("rbind", res)
+    df = as.data.frame(df)
+    df$trait = trait
+    res_trait[[trait]] = df
+  }
+  
+  
+  df = do.call("rbind", res_trait)
+  df = as.data.frame(df)
+  df$flag = flag
+  names(df) = c("prs_cor", "trait", "Type")
+  return(df)
+}
 
-x = fread(fn)
+
+
+p1 <- ggplot(df, aes( y = prs_cor, x = trait, color=  Type)) + geom_boxplot() + theme_light() + ylab("Mean PGS correlation") + xlab("") + guides(x = guide_axis(angle = 90)) 
+
